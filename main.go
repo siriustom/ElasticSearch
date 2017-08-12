c52ae48636c7bca568fd3471ee8f2b36b2ec24e5
siriustom <linxiaoran0210@gmail.com>
2017-08-12 15:20:49 -0700

    5th

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"strconv"
 	"reflect"
+	"context"
+	"cloud.google.com/go/bigtable"
 	elastic "gopkg.in/olivere/elastic.v3"
 	"github.com/pborman/uuid"
 )
@@ -17,8 +19,8 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "windy-ripsaw-176022"
+	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://54.218.64.118:9200"
 )
@@ -78,6 +80,7 @@ func main() {
 func handlerPost(w http.ResponseWriter, r *http.Request) {
 	// Parse from body of request to get a json object.
 	fmt.Println("Received one post request")
+
 	decoder := json.NewDecoder(r.Body)
 	var p Post
 	if err := decoder.Decode(&p); err != nil {
@@ -110,6 +113,29 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+
+	ctx := context.Background()
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
